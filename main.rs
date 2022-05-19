13825d1051b3a56de608eb26db4a7ca587daabc8
rookieCookies <87831835+rookieCookies@users.noreply.github.com>
2022-05-19 03:56:31 +0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,6 +1,6 @@
-use std::{fs::File, io::Read, collections::HashMap};
+use std::{fs::File, io::Read, collections::HashMap, str::Chars};
 
-const CENSOR_TEXT : &'static str = "****";
+const CENSOR_TEXT : &str = "****";
 
 fn main() {
     let mut blacklist_file = match File::open("blacklist.txt") {
@@ -16,42 +16,35 @@ fn main() {
     let mut base_filter = Filter::new();
     Parser::compile_filter(&mut base_filter, &blacklisted_word_string);
 
-    println!("{}", base_filter.filter_out(&"Never gonna give you up".to_string()));
-    println!("{}", base_filter.filter_out(&"Never gonna let you down".to_string()));
-    println!("{}", base_filter.filter_out(&"Never gonna run around and desert you".to_string()));
-    println!("{}", base_filter.filter_out(&"Never gonna make you cry".to_string()));
-    println!("{}", base_filter.filter_out(&"Never gonna say goodbye".to_string()));
-    println!("{}", base_filter.filter_out(&"Never gonna tell a lie and hurt you".to_string()));
+    println!("{}", base_filter.filter_out("Never gonna give you up"));
+    println!("{}", base_filter.filter_out("Never gonna let you down"));
+    println!("{}", base_filter.filter_out("Never gonna run around and desert you"));
+    println!("{}", base_filter.filter_out("Never gonna make you cry"));
+    println!("{}", base_filter.filter_out("Never gonna say goodbye"));
+    println!("{}", base_filter.filter_out("Never gonna tell a lie and hurt you"));
 
 
-    print_in_hierarchy(&base_filter, 0);
+    print_in_hierarchy(&base_filter.trie, 0);
 }
 
 #[derive(Debug)]
 struct Filter {
-    map: HashMap<char, Filter>,
+    trie: Node,
 }
 
 impl Filter {
     fn new() -> Self { 
-        Self { map: HashMap::new() }
-    }
-    
-    fn add_word(&mut self, word: &str) {
-        let mut current_filter = self;
-        for c in word.chars() {
-            current_filter = current_filter.map.entry(c).or_insert(Filter::new());
-        }
+        Self { trie: Node::new() }
     }
 
-    fn filter_out(&self, message: &String) -> String {
+    fn filter_out(&self, message: &str) -> String {
         let mut new_message = String::new();
 
-        for s in Parser::string_to_words(message, " ") {
-            new_message += if Filter::is_blacklisted(self, &s, 0) {
+        for original_word in Parser::string_to_words(message, " ") {
+            new_message += if Filter::is_blacklisted(&self.trie, &mut original_word.to_lowercase().chars()) {
                 CENSOR_TEXT
             } else {
-                &s
+                &original_word
             };
             new_message += " ";
         }
@@ -59,39 +52,53 @@ impl Filter {
         new_message
     }
 
-    fn is_blacklisted(filter: &Filter, word: &String, current_location: usize) -> bool {
-        let converted_word = word.to_lowercase();
-        let char = match converted_word.chars().nth(current_location) {
+    fn is_blacklisted(node: &Node, word_chars: &mut Chars) -> bool {
+        let char = match word_chars.next() {
             Some(c) => c,
             None => return true
         };
-        if filter.map.contains_key(&char) {
-            return Filter::is_blacklisted(filter.map.get(&char).unwrap(), &converted_word, current_location + 1);
+        if node.0.contains_key(&char) {
+            return Filter::is_blacklisted(node.0.get(&char).unwrap(), word_chars);
         }
         false
-
     }
 }
+
+#[derive(Debug)]
+struct Node(HashMap<char, Node>);
+
+impl Node {
+    fn new() -> Self {
+        Self(HashMap::new())
+    }
+    fn add_word(&mut self, word: &str) {
+        let mut current_node = self;
+        for char in word.chars() {
+            current_node = current_node.0.entry(char).or_insert_with(Node::new);
+        }
+    }
+}
+
 
 struct Parser;
 
 impl Parser {
-    fn string_to_words(string: &String, separator: &str) -> Vec<String> {
-        let mut v  = Vec::new();
-        for s in string.split(separator) {
-            v.push(s.to_string())
+    fn string_to_words(string: &str, separator: &str) -> Vec<String> {
+        let mut vector  = Vec::new();
+        for word in string.split(separator) {
+            vector.push(word.to_string())
         }
-        v
+        vector
     }
-    fn compile_filter(filter: &mut Filter, list: &String) {
-        for s in Parser::string_to_words(list, "\n") {
-            filter.add_word(&s.to_lowercase())
+    fn compile_filter(filter: &mut Filter, list: &str) {
+        for word in Parser::string_to_words(list, "\n") {
+            filter.trie.add_word(&word.to_lowercase())
         }
     }
 }
 
-fn print_in_hierarchy(filter: &Filter, indent: usize) {
-    for (c, f) in filter.map.iter() {
+fn print_in_hierarchy(node: &Node, indent: usize) {
+    for (section_char, section_node) in node.0.iter() {
         for i in 0..indent {
             if indent-1 != i {
                 print!("-");
@@ -99,7 +106,7 @@ fn print_in_hierarchy(filter: &Filter, indent: usize) {
                 print!(">");
             }
         }
-        println!("{:?}", c);
-        print_in_hierarchy(f, indent + 1);
+        println!("{:?}", section_char);
+        print_in_hierarchy(section_node, indent + 1);
     }
 }
